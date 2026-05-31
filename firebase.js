98530a10bf6524b9325150e5f6c382a3b16af4e0
jupiterforces <jahongirendev@gmail.com>
2026-05-31 10:40:56 +0500

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,25 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import {
+  getFirestore,
+  setDoc,
+  doc,
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+// Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyCCqvgW8yTfmTHglz3siSLfLJo6NjXd_ws",
+  authDomain: "pulse-5f61d.firebaseapp.com",
+  databaseURL: "https://pulse-5f61d-default-rtdb.firebaseio.com",
+  projectId: "pulse-5f61d",
+  storageBucket: "pulse-5f61d.appspot.com",
+  messagingSenderId: "359964012652",
+  appId: "1:359964012652:web:10722fdfb19d41a1052849",
+  measurementId: "G-QX686G4TT9",
+};
+
+// Init Firebase
+const app = initializeApp(firebaseConfig);
+
+// Firestore exports
+export const db = getFirestore(app);
+export { setDoc, doc };
