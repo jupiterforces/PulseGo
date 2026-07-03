8089bedcd42d32c7ddb8f35cef8d40ec44ed43bc
jupiterforces <jahongirendev@gmail.com>
2026-07-03 23:55:33 +0500

    Add files via upload

---
+++ s1/anatomy.js
@@ -0,0 +1,257 @@
+window.TEST_QUESTIONS = {
+  "Ufemale": [
+    {
+      "q": "A 63-year-old postmenopausal woman comes to the office due to right-sided lower abdominal pain for the last 5 months. The patient has had no changes in bowel or bladder habits, no increase in abdominal girth or bloating, and no early satiety. Physical examination shows a soft abdomen with slight tenderness to palpation over the right lower quadrant. On pelvic examination, the uterus is small and the right adnexa is enlarged. Transvaginal ultrasound shows a normal uterus; a unilateral, 3.5-cm, complex ovarian mass with cystic and solid components; and no free fluid in the pelvis. Surgery is planned to remove the right ovary, including the mass. To avoid excessive bleeding, which of the following structures should be ligated during the oophorectomy?",
+      "a": [
+        "Mesosalpinx",
+        "Round ligament of the uterus",
+        "Suspensory ligament of the ovary",
+        "Transverse cervical ligament",
+        "Uteroovarian ligament"
+      ],
+      "correct": 2
+    },
+    {
+      "q": "A 39-year-old woman is admitted to the hospital for a total abdominal hysterectomy for symptomatic uterine fibroids. The largest fibroid, measuring 8 cm, is located in the lower aspect of the uterus at the level of the cardinal ligament, compressing the bladder anteriorly. The patient has no other medical conditions and no prior surgeries. After the surgery, she develops right-sided back pain. The patient has been able to urinate without difficulty. Injury to which of the following structures is the most likely explanation for this patient's postoperative symptoms?",
+      "a": [
+        "Bladder",
+        "External iliac artery",
+        "Ovarian artery",
+        "Ureter",
+        "Uterine artery"
+      ],
+      "correct": 3
+    },
+    {
+      "q": "A 64-year-old woman comes to the office for evaluation of a vulvar lesion. The patient has had a vulvar plaque for the past year that has slowly increased in size. She underwent menopause at age 52 and has a 30-year smoking history. Vital signs are normal. Physical examination shows a 1-cm nonfriable, white plaque on the right labia majora. The remainder of the physical examination is normal. A biopsy of the lesion is performed and shows squamous cell carcinoma. A sentinel lymph node biopsy is planned for cancer staging. This procedure should target which of the following lymph nodes?",
+      "a": [
+        "Common iliac",
+        "External iliac",
+        "Inguinofemoral",
+        "Obturator",
+        "Paraaortic",
+        "Presacral"
+      ],
+      "correct": 2
+    },
+    {
+      "q": "A 29-year-old woman, gravida 2 para 2, comes to the emergency department with fever, crampy abdominal pain, and right flank pain 8 days after an uncomplicated vaginal delivery. Temperature is 38.3°C (101°F) and blood pressure is 110/60 mmHg. Physical examination shows tenderness in the right lower quadrant and flank. The patient is hospitalized and started on intravenous antibiotics for presumed postpartum endometritis, but her fever persists. Urine and blood cultures show no bacterial growth. CT scan of the abdomen and pelvis reveals right ovarian vein thrombosis. If this patient remains untreated, she is at risk for extension of the thrombus into which of the following vessels?",
+      "a": [
+        "Inferior vena cava",
+        "Right common iliac vein",
+        "Right internal iliac vein",
+        "Right renal vein",
+        "Right uterine vein"
+      ],
+      "correct": 0
+    },
+    {
+      "q": "A 48–year–old woman comes to the office due to painful, heavy menses for the past 6 months. The patient has 4–5 days of heavy bleeding, often soaking through a tampon or sanitary napkin every few hours. Menstrual periods occur every 28–30 days. The patient is not on contraception and has no significant medical history. Vital signs are normal. Urine pregnancy test is negative. After an appropriate workup, a hysterectomy is performed, and the surgical specimen is shown below.\n\nWhich of the following is the most likely diagnosis?",
+      "a": [
+        "Adenomyosis",
+        "Chronic endometritis",
+        "Complete hydatidiform mole",
+        "Endometriosis",
+        "Uterine leiomyoma",
+        "Uterine sarcoma"
+      ],
+      "correct": 0
+    },
+    {
+      "q": "A 15–year–old girl comes to the office due to pelvic and crampy lower abdominal pain that seems to recur at the beginning of each month and resolves after a day or two. The pain began approximately 6 months ago and has significantly worsened each month. The patient has never had a menstrual period. She has never had sexual intercourse. Weight, height, and BMI are average for age and sex. Breast and pubic hair development are at sexual maturity rating (Tanner stage) 5. Digital rectal examination reveals a palpable mass anterior to the rectum. A pregnancy test is negative.\n\nWhich of the following is the most likely diagnosis?",
+      "a": [
+        "Androgen insensitivity syndrome",
+        "Asherman syndrome",
+        "Endometriosis",
+        "Imperforate hymen",
+        "Kallmann syndrome"
+      ],
+      "correct": 3
+    },
+    {
+      "q": "A 28–year–old woman, gravida 1 para 0, at 38 weeks gestation undergoes spontaneous vaginal delivery. After an uncomplicated delivery of the fetus and placenta, a maternal laceration is noted at the posterior vaginal opening through the vaginal mucosa and submucosa.\n\nWhich of the following structures is most likely damaged in this patient?",
+      "a": [
+        "Internal anal sphincter",
+        "Ischiocavernosus muscle",
+        "Levator ani muscle",
+        "Perineal body",
+        "Sacrotuberous ligament"
+      ],
+      "correct": 3
+    },
+    {
+      "q": "A 63–year–old woman comes to the office for evaluation of chronic constipation. For the past 8 months, the patient has had increased straining with bowel movements but no abdominal pain or rectal bleeding. She has 2 children, both delivered via forceps-assisted vaginal delivery. BMI is 31 kg/m2. On digital rectal examination, there are no masses and rectal tone is normal; pelvic examination shows a visible bulge into the vagina with straining. Sensation around the perineum is intact. Fecal occult blood testing is negative.\n\nWhich of the following is the most likely cause of this patient's symptoms?",
+      "a": [
+        "Absence of colonic intramural ganglion cells",
+        "Colonic sensitivity with chronic irregular contractions",
+        "Damage to the levator ani muscle complex",
+        "Defect in the external anal sphincter",
+        "Increased internal anal sphincter parasympathetic tone"
+      ],
+      "correct": 2
+    },
+    {
+      "q": "A 24-year-old primigravid patient at 38 weeks gestation undergoes vaginal delivery of healthy twins after a prolonged labor course. Shortly after the placenta is delivered, profuse vaginal bleeding and a boggy uterus are noted on examination. Uterine massage and uterotonic medications fail to stop the bleeding. The patient is taken to the operating room for a laparotomy. Control of this patient's bleeding can best be achieved through bilateral ligation of which of the following arteries?",
+      "a": [
+        "Common iliac",
+        "External iliac",
+        "Internal iliac",
+        "Internal pudendal",
+        "Ovarian"
+      ],
+      "correct": 2
+    },
+    {
+      "q": "A 29-year-old nulliparous woman comes to the emergency department due to right-sided pelvic pain. The pain is sharp and began suddenly while she was shoveling snow this morning. It is accompanied by nausea and is worse with movement. The patient's last menstrual period was 2 weeks ago. Temperature is 37.8°C (100°F), blood pressure is 100/60 mmHg, and pulse is 92/min. Pelvic ultrasound reveals a normal-sized uterus and left ovary; there is a right adnexal mass measuring 6 cm with absent blood flow to the right ovary. Urine β-hCG is negative. The patient's condition most likely arises from obstructed blood flow through which of the following ligaments?",
+      "a": [
+        "Cardinal ligament",
+        "Infundibulopelvic ligament",
+        "Mesosalpinx",
+        "Round ligament",
+        "Uterosacral ligament"
+      ],
+      "correct": 1
+    },
+    {
+      "q": "A 23-year-old primigravida woman has a vaginal delivery of a 3600 g (7 lb 15 oz) infant after a protracted labor. The patient has had no pain medications or epidural analgesia during delivery. She sustains a second-degree perineal laceration, and repair is planned. Prior to administering local anesthesia, the physician palpates for distinct bony protrusions located on the posterolateral vaginal sidewalls; a firm band can be felt running medially and posteriorly from each of the bilateral bony protrusions. The anesthetic agent is injected around the bony prominence. Which of the following nerves is most likely blocked by this injection?",
+      "a": [
+        "Genitofemoral",
+        "Iliohypogastric",
+        "Inferior gluteal",
+        "Obturator",
+        "Posterior femoral cutaneous",
+        "Pudendal"
+      ],
+      "correct": 5
+    },
+    {
+      "q": "A 54-year-old postmenopausal woman comes to the office due to increasing vaginal pressure for the past 3 months. The patient has had no vaginal bleeding or abnormal discharge. BMI is 18 kg/m². Vital signs are normal. On speculum examination, the anterior vaginal wall bulges with the Valsalva maneuver. On bimanual examination, the uterus is small and mobile. Which of the following is the most likely underlying cause of this patient's presentation?",
+      "a": [
+        "Chronic endometriosis",
+        "Prior uterine artery ligation",
+        "Pudendal nerve injury",
+        "Uterine leiomyomas"
+      ],
+      "correct": 2
+    }
+  ],
+  "Umale": [
+    {
+      "q": "A 31-year-old man comes to the emergency department complaining of right-sided scrotal pain and swelling that has gradually worsened over the last 3 days. His temperature is 38.3°C (101°F). On physical examination, his right hemiscrotum is warm, tender, and erythematous. The cremasteric reflex is present. A scrotal ultrasound reveals a fluid collection consistent with a superficial scrotal abscess. Which of the following lymph node groups is most likely to be tender and swollen?",
+      "a": [
+        "Common iliac",
+        "Inferior mesenteric",
+        "Infraclavicular",
+        "Para-aortic",
+        "Superficial inguinal"
+      ],
+      "correct": 4
+    },
+    {
+      "q": "A 62-year-old man comes to the office due to an elevated prostate-specific antigen level on a screening test. When asked about genitourinary symptoms, the patient says, 'It often takes a bit of time before my urine starts flowing,' but he has no other problems. Abdominal and external genital examinations are unremarkable. Digital rectal examination reveals hard prostate nodules. A biopsy confirms adenocarcinoma, and the patient undergoes a radical prostatectomy. During the surgery, the nerves within the fascia surrounding the gland are inadvertently injured. Which of the following is the most likely consequence of the nerve injury?",
+      "a": [
+        "Detrusor muscle overactivity",
+        "Erectile dysfunction",
+        "External urethral sphincter paralysis",
+        "Fecal incontinence",
+        "Loss of cremasteric reflex",
+        "Loss of penile skin sensation"
+      ],
+      "correct": 1
+    },
+    {
+      "q": "A 70-year-old man comes to the office due to 3 weeks of unrelenting low back pain. He was grocery shopping when he first noticed the pain. The patient reports no trauma or leg weakness but describes having to strain to urinate. He has a history of hypertension and hyperlipidemia and has not seen a doctor in the past 5 years. His wife died a year ago, and he now lives alone. The patient is a retired construction worker and has a history of tobacco and marijuana use. There is tenderness in the lower vertebral area. Imaging study of the spine reveals several osteoblastic lesions in the lumbar vertebrae. Which of the following structures was most likely involved during the spread of this patient's disease?",
+      "a": [
+        "Azygos veins",
+        "Internal iliac lymph nodes",
+        "Pampiniform plexus",
+        "Paraaortic lymph nodes",
+        "Prostatic venous plexus",
+        "Vertebral artery"
+      ],
+      "correct": 4
+    },
+    {
+      "q": "A 6-month-old boy is brought to the urology clinic for follow-up of an undescended right testicle discovered during evaluation in the newborn nursery. Physical examination shows absence of a palpable right testis in the scrotal sac. However, a round mass is palpated superior to the scrotum in the inguinal canal. Orchiopexy, the placement and fixation of the testis in the scrotum, is recommended to the family. During this patient's procedure, the malpositioned testis will most likely be pulled through a physiologic opening in which of the following structures?",
+      "a": [
+        "Conjoint tendon",
+        "External oblique aponeurosis",
+        "Femoral ring",
+        "Rectus muscle sheath",
+        "Transversalis fascia"
+      ],
+      "correct": 1
+    },
+    {
+      "q": "A 21-year-old previously healthy man comes to the emergency department with acute onset of severe left scrotal pain and nausea. The pain started after he returned home from the gym following an intense workout. He denies any trauma, fever, or dysuria. Examination shows a swollen and tender left testis that lies higher than the right testis. Doppler ultrasound shows decreased arterial blood flow in the left spermatic cord entering the testis. Which of the following is the likely origin of the artery involved in this patient's condition?",
+      "a": [
+        "Abdominal aorta",
+        "External iliac artery",
+        "Internal iliac artery",
+        "Internal pudendal artery",
+        "Obturator artery",
+        "Renal artery"
+      ],
+      "correct": 0
+    },
+    {
+      "q": "A 23-year-old man comes to the physician complaining of right-sided testicular swelling. Ultrasonography shows a solid testicular mass. If malignant, this patient's tumor is most likely to spread to which of the following lymph node groups?",
+      "a": [
+        "Superficial inguinal",
+        "Deep inguinal",
+        "External iliac",
+        "Common iliac",
+        "Inferior mesenteric",
+        "Para-aortic"
+      ],
+      "correct": 5
+    },
+    {
+      "q": "A 2-week-old boy is brought to the office for his first newborn evaluation. He has a painless enlarged, fluctuant left hemiscrotum that transilluminates brightly. The most likely cause of this patient's condition is a swelling in which of the following locations?",
+      "a": [
+        "Caput of the epididymis",
+        "Cremasteric fascia",
+        "External spermatic fascia",
+        "Internal spermatic fascia",
+        "Pampiniform plexus",
+        "Tunica albuginea",
+        "Tunica vaginalis"
+      ],
+      "correct": 6
+    },
+    {
+      "q": "A 16-year-old boy is brought to the emergency department due to severe right groin pain and unilateral scrotal swelling. Orchiectomy specimen shows hemorrhagic infarction of the testis. Which of the following is the most likely mechanism of this patient's acute testicular pain?",
+      "a": [
+        "Abnormal collection of fluid within the scrotum",
+        "Anatomic defect causing increased mobility of the testis",
+        "Clonal proliferation of testicular germ cells",
+        "Migration of bacteria from the urinary tract",
+        "Postsurgical clotting of the pampiniform plexus"
+      ],
+      "correct": 1
+    },
+    {
+      "q": "A 63-year-old man has urinary hesitancy, straining, and incomplete bladder emptying due to benign prostatic hyperplasia. He reports significant improvement after starting finasteride. A therapeutic effect on which structure is most likely responsible for this symptomatic improvement?",
+      "a": [
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F"
+      ],
+      "correct": 3
+    },
+    {
+      "q": "A 45-year-old man is brought to the emergency department after a high-speed motor vehicle collision. He has a pelvic fracture, suprapubic tenderness, bladder fullness, and inability to void. Which portion of the urogenital tract is most likely injured?",
+      "a": [
+        "A",
+        "B",
+        "C",
+        "D",
+        "E"
+      ],
+      "correct": 2
+    }
+  ]
+};
